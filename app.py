06039cb8bcfd91d9047fec3faa0fa6ec984c417d
Érico Penteado <109689624+ericopent@users.noreply.github.com>
2023-10-02 17:46:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,92 @@
 import pandas as pd
 import streamlit as st
+import datetime
+import gspread
+from gspread_dataframe import set_with_dataframe
+st.set_page_config(
+    layout="wide",
+    page_title = 'Fut Iate',
+    page_icon='https://www.icrj.com.br/iate/images/logo/logo60.png')
+    
+st.markdown("""
+    <style>
+    p {
+        font-size:20px;
+        place-items: center;
 
-# Read in data from the Google Sheet.
-# Uses st.cache_data to only rerun when the query changes or after 10 min.
-@st.cache_data(ttl=600)
-def load_data(sheets_url):
-    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
-    return pd.read_csv(csv_url)
+    }
+    
+    .st-af {
+        font-size: 19px;
+    }
+    
+    .css-10trblm.eqr7zpz0 {
+        
+        font-size:25px;
+    }
+    
+    code {
+        color: rgb(9, 171, 59);
+        overflow-wrap: break-word;
+        font-size: 20px;
+    }   
+    
+    .stMarkdown {
+    display: grid;
+    place-items: center;
+    }
+    
+    button{
+        display: grid;
+        place-items: center;
+        
+    }
+    
+    .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] {
+        font-size:24px;
+        text-align: center;
+        place-items: center;
 
-df = load_data(st.secrets["public_gsheets_url"])
+    }
+    
+    .css-q8sbsg.eqr7zpz4 {
+        
+        font-size:24px;
+        text-align: center;
+        place-items: center;
+        
+    }
+    
+    .css-10trblm.eqr7zpz0 {
+        
+        font-size:40px;
+        text-align: center;
+        place-items: center;
+        
+    }
+    
+    .css-3mmywe.e15ugz7a0 {
+        
+        place-items: center;
+        align-content: center;
+        
+    }
+    
+    h2 {text-align: center;}
+    </style>
+    """, unsafe_allow_html=True)
 
-st.write(df)
+service_account = "service-account-key.json"
+
+def read_data(sheet = "FutIate", tab = "Ranking"):
+    
+    gc = gspread.service_account(filename=service_account)
+
+    sh = gc.open(sheet)    
+    
+    worksheet = sh.worksheet(tab)
+
+    df = pd.DataFrame(worksheet.get_all_records())
+
+    return(df)
+  
